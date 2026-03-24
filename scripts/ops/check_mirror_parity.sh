1298800f600cda18cd0d0f858e0bd8d8948ef0ff
John Malone <john.malone@gmail.com>
2026-03-24 17:23:17 -0500

    fix(ops): parity check uses source/destination PATs

--- scripts/ops/check_mirror_parity.sh
+++ scripts/ops/check_mirror_parity.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+: "${SOURCE_TOKEN:?SOURCE_TOKEN is required}"
+: "${DEST_TOKEN:?DEST_TOKEN is required}"
+
 pairs=(
   "Pro777/alcove-private Spitfire-Cowboy/alcove-private develop"
   "Pro777/alcove-demo Spitfire-Cowboy/alcove-demo main"
 )
+
+src_url() {
+  local repo="$1"
+  echo "https://x-access-token:${SOURCE_TOKEN}@github.com/${repo}.git"
+}
+
+dst_url() {
+  local repo="$1"
+  echo "https://x-access-token:${DEST_TOKEN}@github.com/${repo}.git"
+}
 
 fail=0
 
@@ -12,13 +25,13 @@ for row in "${pairs[@]}"; do
   read -r source_repo dest_repo default_branch <<<"$row"
   echo "Checking ${source_repo} -> ${dest_repo}"
 
-  src_heads=$(git ls-remote --heads "git@github.com:${source_repo}.git" | wc -l | tr -d ' ')
-  dst_heads=$(git ls-remote --heads "git@github.com:${dest_repo}.git" | wc -l | tr -d ' ')
-  src_tags=$(git ls-remote --tags "git@github.com:${source_repo}.git" | wc -l | tr -d ' ')
-  dst_tags=$(git ls-remote --tags "git@github.com:${dest_repo}.git" | wc -l | tr -d ' ')
+  src_heads=$(git ls-remote --heads "$(src_url "$source_repo")" | wc -l | tr -d ' ')
+  dst_heads=$(git ls-remote --heads "$(dst_url "$dest_repo")" | wc -l | tr -d ' ')
+  src_tags=$(git ls-remote --tags "$(src_url "$source_repo")" | wc -l | tr -d ' ')
+  dst_tags=$(git ls-remote --tags "$(dst_url "$dest_repo")" | wc -l | tr -d ' ')
 
-  src_sha=$(git ls-remote --heads "git@github.com:${source_repo}.git" "$default_branch" | awk '{print $1}')
-  dst_sha=$(git ls-remote --heads "git@github.com:${dest_repo}.git" "$default_branch" | awk '{print $1}')
+  src_sha=$(git ls-remote --heads "$(src_url "$source_repo")" "$default_branch" | awk '{print $1}')
+  dst_sha=$(git ls-remote --heads "$(dst_url "$dest_repo")" "$default_branch" | awk '{print $1}')
 
   echo "heads ${src_heads}/${dst_heads} tags ${src_tags}/${dst_tags} ${default_branch} ${src_sha}/${dst_sha}"
 
